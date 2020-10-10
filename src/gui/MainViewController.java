2f99914cc0904488f190ec1a130622aea24957ee
anderson-lda <anderson.lima.araujo@hotmail.com>
2020-10-10 17:28:45 -0300

    About view

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
@@ -26,12 +35,29 @@ public class MainViewController implements Initializable {
 	}
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
+		loadView("/gui/About.fxml");
 	}
 	
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 		
+	}
+	
+	private synchronized void loadView(String absoluteName) { //como interfaces graficas sao diversas threads, o sync garante q o processamento nao sera interrompido
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0); //como serão apagados, é preciso guardá-los em uma var pra usar depois
+			mainVBox.getChildren().clear(); //apaga todos os nodes anteriores
+			mainVBox.getChildren().add(mainMenu); //add o menu da nova tela
+			mainVBox.getChildren().addAll(newVBox.getChildren()); //add os componentes da nova tela
+		} catch (IOException e) {
+			Alerts.showAlert("IOException", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
